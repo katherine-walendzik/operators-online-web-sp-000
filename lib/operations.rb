94615e08fb4b96a77606077849cd4dfd8d188f61
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-21 18:15:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
-  if 40 < "speed" < 60
-    return false
-  else
+  if 60 < speed < 40
     return true
+  else
+    return false
   end
 end
 
 
 
 def not_safe?(speed)
-	40 < speed < 60 ? false : true
+	60 < speed < 40 ? true : false
 end
